7067f43dd915f608ede8d4ee18e5884294d90352
Janna Fox <jannafox@gmail.com>
2020-08-09 19:20:01 -0600

    MVP

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function processFirstItem(stringList, callback) {
   return callback(stringList[0])
 }
 
+
 // ⭐️ Example Challenge END ⭐️
 
 
@@ -28,9 +29,15 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * Counter one is a closure, counter two is not.
+ * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
+ * a- Counter 1 b- The variable "count" is inside the function.
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * 
+ * With counter1 you'd You'd want to use counter2 when you'd need your variables in a global scope rather than a local scope.
  *
 */
 
@@ -56,11 +63,16 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+// this function only needs to return a random number, so it doesn't need an inner function or multiple levels of scope.
 
-    /*Code Here*/
-
+function inning() {
+  return Math.floor(Math.random() * 3);
 }
+
+console.log(inning());
+console.log(inning());
+console.log(inning());
+
 
 /* Task 3: finalScore()
 
@@ -76,11 +88,25 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning, numInnings){
+  let home = 0;
+  let away = 0;
 
-  /*Code Here*/
+  for(let i = 1; i <= numInnings; i++){
+    home += inning();
+    away += inning(); 
+  }
 
+  let Obj = { 
+    "Home": home,
+    "Away": away
+  }
+  return Obj;
 }
+const scoreBoard = finalScore(inning, 9);
+console.log(finalScore(inning, 9))
+console.log(finalScore(inning, 9))
+console.log(finalScore(inning, 9))
 
 /* Task 4: 
 
@@ -103,8 +129,47 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+// function scoreboard(getInningScore,inning,numberOfInnings) {
+//   for (i = 0; i <= totallinnings; i++) {
+//     console.log(inning);
+//   }
+//   if (i = 0) {
+//     console.log ()
+//   }
+//   /* CODE HERE */
+// }
+
+function scoreboard(getInningScore, inning, num) {
+  let score = {};
+  let finalScore = getInningScore(0, 0);
+  let inningNum = [
+    "1st",
+    "2nd",
+    "3rd",
+    "4th",
+    "5th",
+    "6th",
+    "7th",
+    "8th",
+    "9th",
+  ];
+  console.log('Inning: Away Team - Home Team')
+  for (i = 0; i < num; i++) {
+    score = getInningScore(inning(), inning());
+    finalScore["Home"] += score["Home"];
+    finalScore["Away"] += score["Away"];
+    console.log(`${inningNum[i]} inning: ${score["Away"]} - ${score["Home"]}`);
+  }
+  console.log(`Final Score: ${finalScore["Away"]} - ${finalScore["Home"]}`);
 }
+
+function getInningScore(val1, val2) {
+  return { Home: val1, Away: val2 };
+}
+
+console.log("Task 5:");
+scoreboard(getInningScore, inning, 9);
+console.log("\n");
+
 
 
